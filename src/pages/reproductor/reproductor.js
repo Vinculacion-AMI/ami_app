a60b386244d147f61ad04e32a2348b9dd860c8eb
jacksonp12 <jacksondemente@gmail.com>
2021-11-22 14:26:57 -0500

    Video

--- src/pages/reproductor/reproductor.js
+++ src/pages/reproductor/reproductor.js
@@ -5,9 +5,6 @@ import Button from "@material-ui/core/Button";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import { Grid, Container } from "@material-ui/core";
-import CardMedia from "@material-ui/core/CardMedia";
-import Card from "@material-ui/core/Card";
-import { Carousel } from "react-responsive-carousel";
 import AppNavBar from "../../components/navbar";
 
 import { useHistory } from "react-router-dom";
@@ -41,42 +38,13 @@ function Reproductor() {
       >
 
         <Container fixed >
-          <Grid spacing={2} container >
-            <Grid item md={3} xs={1}></Grid>
-            <Grid align="center" item xs={10} md={20} sm={12} >
-              <Carousel dynamicHeight={true} emulateTouch={true} >
-                <Card boxShadow={6} className={classes.root} >
                   <CardContent style={{ backgroundColor: "#6495ED" }}>
                     <Typography gutterBottom variant="h5" component="h2" >
                       <div className="reproductor" >
-                        <ReactPlayer controls url='https://www.youtube.com/watch?v=TyOWmMUsJYU'></ReactPlayer>
+                        <ReactPlayer controls url='../../../images/Vdeo.mp4'></ReactPlayer>
                       </div>
                     </Typography>
                   </CardContent>
-                </Card>
-                <Card className={classes.root}>
-                  <CardContent style={{ backgroundColor: "#6495ED" }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      <div className="reproductor">
-                        <ReactPlayer controls url='https://www.youtube.com/watch?v=-sHbCdJ1xw8'></ReactPlayer>
-                      </div>
-
-                    </Typography>
-                  </CardContent>
-                </Card>
-                <Card className={classes.root}>
-                  <CardContent style={{ backgroundColor: "#6495ED" }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      <div className="reproductor">
-                        <ReactPlayer controls url='https://www.youtube.com/watch?v=bO23pUTXyA4&t=8s&ab_channel=lunacreciente'></ReactPlayer>
-                      </div>
-
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Carousel>
-            </Grid>
-          </Grid>
         </Container>
       </Grid>
       <Button
